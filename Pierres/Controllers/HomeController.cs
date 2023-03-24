88158e1c1e99dc036a8f8a56d4bc1e355744fc3b
David Jandron <jandron1211@gmail.com>
2023-03-24 10:25:00 -0700

    ADD code to Flavors Controller

---
+++ Pierres/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Pierres.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pierres.Controllers
+{
+    public class HomeController : Controller
+    {
+      private readonly PierresContext _db;
+
+      public HomeController(PierresContext db)
+      {
+        _db = db;
+      }
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      { 
+        Treat[] treats = _db.Treats.ToArray();
+        Flavor[] flavors = _db.Flavors.ToArray();
+        Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+        model.Add("treats", treats);
+        model.Add("flavors", falvors);
+        return View(model);
+      }
+    }
+}
