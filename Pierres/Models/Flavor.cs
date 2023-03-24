0a560b610fa72d07ee35b6c0a6654f7ee25dcef8
David Jandron <jandron1211@gmail.com>
2023-03-24 09:00:42 -0700

    ADD project folders and files, and start models

---
+++ Pierres/Models/Flavor.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Pierres.Models
+{
+  public class Flavor
+  {
+    public int FlavorId { get; set; }
+    [Required(ErrorMessage = "The flavors field can't be empty!")]
+    public string FlavorType { get; set; }
+    public List<Treat> Treat { get; set; }
+    public List<TreatFlavor> JoinFlavor { get; }
+  }
+}
---
+++ Pierres/Models/Treat.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Pierres.Models
+{
+  public class Treat
+  {
+    public int TreatId { get; set; }
+    [Required(ErrorMessage = "No treats have been added!")]
+    public string TreatType { get; set; }
+    public Flavor Flavor { get; set; }
+    public List<TreatFlavor> JoinFlavor { get; } 
+  }
+}
